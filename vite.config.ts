be7c2b62298870de581d34eef368b3aea2808d54
Rafael <rafael_guliev94@list.ru>
2023-02-28 14:09:26 +0300

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ export default defineConfig({
     assetsInclude: ['**/*.gltf', '**/*.mp3', '**/*.png'],
     build: {
         outDir: 'docs'
-    }
+    },
+    base: 'https://github.com/likeavenus/threejs-test',
 })
