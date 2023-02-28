908b6cebb990dbd6afb94f2b9f8ab7ed2618d831
Rafael <rafael_guliev94@list.ru>
2023-02-28 16:54:53 +0300

    remove useless code

--- src/components/human.ts
+++ src/components/human.ts
@@ -3,14 +3,6 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import humanModel from './assets/human/human.gltf';
 
 export const getHuman = (scene: THREE.Scene) => {
-    // const loadingManager = new THREE.LoadingManager();
-    // const preloaderElem = document.querySelector('.preloader');
-    // loadingManager.onStart = () => {
-    //     preloaderElem?.classList.add('active');
-    // };
-    // loadingManager.onLoad = () => {
-    //     preloaderElem?.classList.remove('active');
-    // }
     const loader = new GLTFLoader();
     loader.load(humanModel, (gltf) => {
         gltf.scene.position.y = 1;
